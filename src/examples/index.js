a9bf687101006da376f63b0e350569d59645cbc4
Ustym <ustym.ukhman@tbwa-x.com>
2024-04-26 02:19:45 +0300

    Add Examples Page

---
+++ src/examples/index.js
@@ -0,0 +1,48 @@
+/** @type {HTMLAnchorElement} */
+let currentAnchor = null;
+
+/** @type {() => void} */
+let destroyCurrent = () => void 0;
+
+const canvas = (
+    /** @type {HTMLCanvasElement} */
+    (document.getElementById("example"))
+);
+
+const codeButton = (
+    /** @type {HTMLButtonElement} */
+    (document.getElementById("code"))
+);
+
+const runExample = async () =>
+{
+    const example = location.hash.slice(1);
+    const same = example === currentAnchor?.dataset.example;
+
+    if (same || !example) return;
+    destroyCurrent();
+
+    /** @type {HTMLAnchorElement} */
+    const anchor = document.querySelector(`a[data-example=\"${example}\"]`);
+
+    currentAnchor?.classList.remove("active");
+    codeButton.classList.remove("hidden");
+
+    anchor?.classList.add("active");
+    currentAnchor = anchor || null;
+
+    const { run, destroy } = await import(`./${example}/index.js`);
+    destroyCurrent = destroy;
+    run(canvas);
+};
+
+function openExample()
+{
+    window.open(`https://github.com/UstymUkhman/uwal/blob/main/src/examples/${
+        location.hash.slice(1)
+    }/index.js`, "_blank");
+}
+
+codeButton.addEventListener("click", openExample, false);
+addEventListener("hashchange", runExample, false);
+runExample();
--- src/lessons/README.md
+++ src/lessons/README.md
@@ -2,6 +2,6 @@
 
 ## [Table of Contents](https://webgpufundamentals.org/)
 
-- [Fundamentals](https://webgpufundamentals.org/webgpu/lessons/webgpu-fundamentals.html) | [UWAL](./fundamentals/index.js)
-- [Inter-stage Variables](https://webgpufundamentals.org/webgpu/lessons/webgpu-inter-stage-variables.html) | [UWAL](./inter-stage-variables/index.js)
-- [Uniforms](https://webgpufundamentals.org/webgpu/lessons/webgpu-uniforms.html) | [UWAL](./uniforms/index.js)
+- [Fundamentals](https://webgpufundamentals.org/webgpu/lessons/webgpu-fundamentals.html) | [Code](./fundamentals/index.js)
+- [Inter-stage Variables](https://webgpufundamentals.org/webgpu/lessons/webgpu-inter-stage-variables.html) | [Code](./inter-stage-variables/index.js)
+- [Uniforms](https://webgpufundamentals.org/webgpu/lessons/webgpu-uniforms.html) | [Code](./uniforms/index.js)
