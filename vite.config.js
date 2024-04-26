64e750266ab6915e48465a1c24bbf56f36d2291d
Ustym <ustym.ukhman@tbwa-x.com>
2024-04-26 15:35:59 +0300

    Update Build Config

--- vite.config.js
+++ vite.config.js
@@ -3,46 +3,66 @@ import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 import { name, version } from "./package.json";
 
-export default({ mode }) => defineConfig(
+/** @param {{ mode: string }} */
+export default({ mode }) =>
 {
-    build:
-    {
-        sourcemap: true,
-        outDir: "build",
+    const config = mode !== "lib" && { base: "./" };
 
-        lib:
+    const build = mode === "lib"
+        ? {
+            outDir: "build",
+            lib:
+            {
+                name: name.toUpperCase(),
+                entry: resolve("lib/UWAL.js")
+            }
+        } : {
+            rollupOptions:
+            {
+                input:
+                {
+                    app: "./examples.html"
+                }
+            }
+        };
+
+    return defineConfig(
+    {
+        ...config,
+
+        build:
         {
-            name: name.toUpperCase(),
-            entry: resolve("lib/UWAL.js")
+            ...build,
+            sourcemap: true
+        },
+
+        plugins:
+        [
+            glsl(
+            {
+                root: "/lib/shaders/",
+                compress: mode === "production"
+            })
+        ],
+
+        define:
+        {
+            VERSION: JSON.stringify(version)
+        },
+
+        resolve:
+        {
+            alias:
+            {
+                "@": resolve("lib")
+            }
+        },
+
+        server:
+        {
+            host: "0.0.0.0",
+            open: false,
+            port: 8080
         }
-    },
-
-    plugins:
-    [
-        glsl(
-        {
-            root: "/lib/shaders/",
-            compress: mode === "production"
-        })
-    ],
-
-    define:
-    {
-        VERSION: JSON.stringify(version)
-    },
-
-    resolve:
-    {
-        alias:
-        {
-            "@": resolve("lib")
-        }
-    },
-
-    server:
-    {
-        host: "0.0.0.0",
-        open: false,
-        port: 8080
-    }
-});
+    });
+};
