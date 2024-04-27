9c1ad02e81fbe5849817b82e4161571411988c4e
Ustym <ustym.ukhman@tbwa-x.com>
2024-04-27 03:33:33 +0300

    Add Lessons Preview

--- src/lessons/index.js
+++ src/lessons/index.js
@@ -1,1 +1,11 @@
-import(`./${document.getElementById("lesson").dataset.lesson}/index.js`);
+const runLesson = () => import(`./${location.hash.slice(1)}/index.js`);
+
+document.getElementById("code").addEventListener("click", () =>
+{
+    window.open(`https://github.com/UstymUkhman/uwal/blob/main/src/lessons/${
+        location.hash.slice(1)
+    }/index.js`, "_blank");
+}, false);
+
+addEventListener("hashchange", runLesson, false);
+runLesson();
