5ce593eab61d05e1cd90c72d207d028384202b5a
Ustym <ustym.ukhman@tbwa-x.com>
2024-04-27 03:34:50 +0300

    Update Build Files

---
+++ dist/lessons/assets/index-OLh325Wa.js
@@ -0,0 +1,36 @@
+import{U as n}from"./UWAL-DFLFQeyv.js";var h=`struct VertexOutput
+{
+    @builtin(position) position: vec4f
+};
+
+@vertex fn vertex(@builtin(vertex_index) index: u32) -> VertexOutput
+{
+    let position = array(
+        vec2f( 0.0,  0.5), 
+        vec2f(-0.5, -0.5), 
+        vec2f( 0.5, -0.5)  
+    );
+
+    var output: VertexOutput;
+
+    output.position = vec4f(position[index], 0.0, 1.0);
+    return output;
+}`,g=`@fragment fn fragment(@builtin(position) position: vec4f) -> @location(0) vec4f
+{
+    let red = vec4f(1, 0, 0, 1);
+    let cyan = vec4f(0, 1, 1, 1);
+
+    let grid = vec2u(position.xy) / 8;
+    let checker = (grid.x + grid.y) % 2 == 1;
+
+    return select(red, cyan, checker);
+}`;/**
+ * @module Inter-stage Variables
+ * @author Ustym Ukhman <ustym.ukhman@gmail.com>
+ * @description This lesson is reproduced from WebGPU Inter-stage Variables
+ * {@link https://webgpufundamentals.org/webgpu/lessons/webgpu-inter-stage-variables.html}&nbsp;
+ * and developed by using a version listed below. Please note that this code
+ * may be simplified in future releases thanks to new library APIs.
+ * @version 0.0.2
+ * @license MIT
+ */(async function(t){let e;try{e=new(await n.RenderPipeline(t,"Checkerboard Triangle Encoder"))}catch(r){alert(r)}const i=e.CreateColorAttachment(void 0,"clear","store",[.3,.3,.3,1]),o=e.CreateRenderPassDescriptor([i],"Checkerboard Triangle Render Pass"),a=e.CreateShaderModule(h,"Triangle Shader"),c=e.CreateShaderModule(g,"Checkerboard Shader"),d=e.CreateVertexState(a),l=e.CreateFragmentState(c),s=e.CreateRenderPipeline({label:"Checkerboard Triangle Pipeline",vertex:d,fragment:l});function u(){n.SetCanvasSize(t.width,t.height),o.colorAttachments[0].view=n.CurrentTextureView,e.Render(o,s,3)}new ResizeObserver(r=>{for(const f of r){const{inlineSize:v,blockSize:p}=f.contentBoxSize[0];n.SetCanvasSize(v,p)}u()}).observe(t)})(document.getElementById("lesson"));
