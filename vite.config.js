5e47e19b29123b369b1e9f759a3923deb0e614e9
Ustym <ustym.ukhman@tbwa-x.com>
2024-05-01 13:22:34 +0300

    Update Examples Styles

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default({ mode }) =>
             glsl(
             {
                 root: "/lib/shaders/",
+                defaultExtension: "wgsl",
                 compress: mode !== "development"
             })
         ],
