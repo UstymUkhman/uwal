4f9abf946228a9515d6900cb36656f046d83bfa4
Ustym <ustym.ukhman@tbwa-x.com>
2024-05-07 19:20:51 +0300

    Add "wgpu-matrix" Library

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite-plugin-glsl/ext" />
 /// <reference types="@webgpu/types" />
+/// <reference types="wgpu-matrix" />
 /// <reference types="vite/client" />
 
 declare const VERSION: string;
