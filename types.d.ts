c928882a0d60eee687aaf43b63241ba3b06f5698
UstymUkhman <ustym.ukhman@gmail.com>
2024-07-12 13:07:37 +0300

    Update Shape And `wgpu-matrix` Types

--- types.d.ts
+++ types.d.ts
@@ -7,9 +7,14 @@
 declare type Mat3 = import("wgpu-matrix").Mat3;
 declare type Mat4 = import("wgpu-matrix").Mat4;
 declare type Quat = import("wgpu-matrix").Quat;
+
 declare type Vec2 = import("wgpu-matrix").Vec2;
 declare type Vec3 = import("wgpu-matrix").Vec3;
 declare type Vec4 = import("wgpu-matrix").Vec4;
+
+declare type Vec2n = import("wgpu-matrix").Vec2n;
+declare type Vec3n = import("wgpu-matrix").Vec3n;
+declare type Vec4n = import("wgpu-matrix").Vec4n;
 
 // WebGPU Context Configuration Options:
 declare type ConfigurationOptions =
