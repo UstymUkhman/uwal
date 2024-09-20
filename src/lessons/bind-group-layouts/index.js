3e9536b0ff4cdb36e46769e0bead6eaa6c1f6c55
UstymUkhman <ustym.ukhman@gmail.com>
2024-09-20 19:20:07 +0300

    Update Bind Group Layouts Lesson With Dynamic Offsets

--- src/lessons/bind-group-layouts/index.js
+++ src/lessons/bind-group-layouts/index.js
@@ -5,20 +5,23 @@
  * {@link https://webgpufundamentals.org/webgpu/lessons/webgpu-bind-group-layouts.html}&nbsp;
  * and developed by using a version listed below. Please note that this code
  * may be simplified in future thanks to more recent library APIs.
- * @version 0.0.8
+ * @version 0.0.9
  * @license MIT
  */
 
 import { UWAL, Shaders, Color } from "@/index";
 import Shader from "./Texture.wgsl";
+import Double from "./Double.wgsl";
 
 (async function(canvas)
 {
     /** @type {Renderer} */ let Renderer;
+    /** @type {Computation} */ let Computation;
 
     try
     {
         Renderer = new (await UWAL.RenderPipeline(canvas, "Bind Group Layouts"));
+        Computation = new (await UWAL.ComputePipeline(canvas, "Dynamic Offsets"));
     }
     catch (error)
     {
@@ -82,10 +85,61 @@ import Shader from "./Texture.wgsl";
         )
     );
 
-    function render()
-    {
-        Renderer.Render(6);
-    }
+    Computation.CreatePipeline({
+        module: Renderer.CreateShaderModule(Double),
+        layout: Renderer.CreatePipelineLayout(
+            Renderer.CreateBindGroupLayout(
+                Array.from({ length: 3 }).fill({
+                    visibility: GPUShaderStage.COMPUTE,
+                    buffer: {
+                        hasDynamicOffset: true,
+                        type: 'storage'
+                    }
+                })
+            )
+        )
+    });
+
+    const input = new Float32Array(64 * 3);
+
+    input.set([1, 3, 5]);
+    input.set([11, 12, 13], 64);
+
+    const computeBuffer = Computation.CreateBuffer({
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST,
+        size: input.byteLength
+    });
+
+    const resultBuffer = Computation.CreateReadableBuffer(input.byteLength);
+
+    Computation.WriteBuffer(computeBuffer, input);
+
+    Computation.SetBindGroups(
+        Computation.CreateBindGroup(
+            Computation.CreateBindGroupEntries(
+                Array.from({ length: 3 }).fill({
+                    buffer: computeBuffer,
+                    size: 256
+                })
+            )
+        ),
+        [[0, 256, 512]]
+    );
+
+    Computation.Workgroups = 3;
+    Computation.Compute();
+
+    Computation.CopyBufferToBuffer(computeBuffer, resultBuffer, resultBuffer.size);
+    Computation.SubmitCommandBuffer();
+
+    await resultBuffer.mapAsync(GPUMapMode.READ);
+    const result = new Float32Array(resultBuffer.getMappedRange());
+
+    console.log('src0:', input.slice(0, 3));
+    console.log('src1:', input.slice(64, 64 + 3));
+    console.log('dst:', result.slice(128, 128 + 3));
+
+    resultBuffer.unmap();
 
     const observer = new ResizeObserver(entries =>
     {
@@ -95,7 +149,7 @@ import Shader from "./Texture.wgsl";
             Renderer.SetCanvasSize(inlineSize, blockSize);
         }
 
-        render();
+        Renderer.Render(6);
     });
 
     observer.observe(canvas);
