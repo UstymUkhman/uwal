02e0f2106bf7bce6f117595c34385b1c6a3b1b6f
Ustym <ustym.ukhman@tbwa-x.com>
2024-11-02 16:41:25 +0200

    Init Text Rendering Example

---
+++ src/examples/text-rendering/index.js
@@ -0,0 +1,60 @@
+/**
+ * @example Text Rendering
+ * @author Ustym Ukhman <ustym.ukhman@gmail.com>
+ * @description This example is inspired by astiopin's "webgl_fonts"
+ * {@link https://astiopin.github.io/webgl_fonts}&nbsp;
+ * and developed by using a version listed below. Please note that this code
+ * may be simplified in future thanks to more recent library APIs.
+ * @version 0.0.9
+ * @license MIT
+ */
+
+import { UWAL } from "@/index";
+
+/** @type {number} */ let raf;
+/** @type {ResizeObserver} */ let observer;
+
+/** @param {HTMLCanvasElement} canvas */
+export async function run(canvas)
+{
+    /** @type {Renderer} */ let Renderer;
+
+    try
+    {
+        Renderer = new (await UWAL.RenderPipeline(canvas, "Text Rendering"));
+    }
+    catch (error)
+    {
+        alert(error);
+    }
+
+    Renderer.CreatePipeline();
+
+    function render()
+    {
+        raf = requestAnimationFrame(render);
+        Renderer.Render(0);
+    }
+
+    observer = new ResizeObserver(entries =>
+    {
+        for (const entry of entries)
+        {
+            const { inlineSize, blockSize } = entry.contentBoxSize[0];
+            Renderer.SetCanvasSize(inlineSize, blockSize);
+        }
+
+        cancelAnimationFrame(raf);
+        raf = requestAnimationFrame(render);
+    });
+
+    observer.observe(canvas);
+}
+
+export function destroy()
+{
+    UWAL.OnDeviceLost = () => void 0;
+    cancelAnimationFrame(raf);
+    observer.disconnect();
+    UWAL.Destroy();
+}
