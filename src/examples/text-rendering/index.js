a29957985c419daa44cc6ac7507491dfbeb8e78b
Ustym <ustym.ukhman@tbwa-x.com>
2024-12-07 20:40:39 +0200

    Update TextRendering Example

--- src/examples/text-rendering/index.js
+++ src/examples/text-rendering/index.js
@@ -9,12 +9,11 @@
  * @license MIT
  */
 
-// import RegularTexture from "/assets/fonts/roboto-regular.png";
-// import RegularData from "/assets/fonts/roboto-regular.json";
+import RegularTexture from "/assets/fonts/roboto-regular.png";
+import RegularData from "/assets/fonts/roboto-regular.json";
 import BoldTexture from "/assets/fonts/roboto-bold.png";
 import BoldData from "/assets/fonts/roboto-bold.json";
 import { UWAL, Shaders, Color, Text } from "@/index";
-import { mat3 } from "wgpu-matrix";
 
 /** @type {number} */ let raf;
 /** @type {ResizeObserver} */ let observer;
@@ -24,13 +23,8 @@ export async function run(canvas)
 {
     /** @type {Renderer} */ let Renderer;
 
-    canvas.style.position = "absolute";
-    canvas.style.height = `${400}px`;
-    canvas.style.width = `${700}px`;
-    canvas.style.top = "115.875px";
-    canvas.style.left = "241.5px";
-
-    // const availableFeatures = await UWAL.SetRequiredFeatures("dual-source-blending");
+    // Enable "Dual Source Blending" https://caniuse.com/?search=dual-source-blending:
+    // const { size: dsb } = (await UWAL.SetRequiredFeatures("dual-source-blending"));
 
     try
     {
@@ -41,79 +35,61 @@ export async function run(canvas)
         alert(error);
     }
 
-    const module = Renderer.CreateShaderModule(Shaders.Text);
+    const loadFontTexture = (text, src) => new Promise(resolve =>
+    {
+        const texture = new Image(); texture.src = src;
+        texture.onload = () => text.SetFontTexture(texture).then(resolve);
+    });
 
+    const module = Renderer.CreateShaderModule(Shaders.Text);
     // const color = Renderer.CreateBlendComponent("add", "one", "one-minus-src1");
-    // const target = Renderer.CreateTargetState(void 0, { color, alpha: {} });
+    const target = Renderer.CreateTargetState(void 0, /* (dsb && { color, alpha: {} }) || */ void 0);
 
     const layout = Renderer.CreateVertexBufferLayout(
         ["position", "texture", "size"], void 0, "textVertex"
     );
 
-    const colorAttachment = Renderer.CreateColorAttachment();
-    colorAttachment.clearValue = new Color(0xffffff).rgba;
-    Renderer.CreatePassDescriptor(colorAttachment);
+    // const colorAttachment = Renderer.CreateColorAttachment();
+    // colorAttachment.clearValue = new Color(0xffffff).rgba;
+    // Renderer.CreatePassDescriptor(colorAttachment);
 
     Renderer.CreatePipeline({
-        fragment: Renderer.CreateFragmentState(module, "textFragment" /*, target */),
+        fragment: Renderer.CreateFragmentState(module, "textFragment", target),
         vertex: Renderer.CreateVertexState(module, "textVertex", layout)
     });
 
-    let text, textUniform;
+    const Title = new Text({
+        color: new Color(0x005a9c),
+        background: new Color(),
+        renderer: Renderer,
+        font: BoldData,
+        size: 144
+    });
 
-    const boldTexture = new Image();
-    boldTexture.src = BoldTexture;
+    const Subtitle = new Text({
+        color: new Color(0xffffff),
+        background: new Color(),
+        renderer: Renderer,
+        font: RegularData,
+        size: 24
+    });
 
-    boldTexture.onload = async () =>
+    loadFontTexture(Title, BoldTexture).then(async () =>
     {
-        const fontColor = new Color();
-
-        text = new Text({
-            renderer: Renderer,
-            color: fontColor,
-            font: BoldData,
-            lineGap: 0.2,
-            size: 144
-        });
-
-        await text.SetFontTexture(boldTexture);
-        textUniform = text.Write("UWAL", [-150, 0]);
-
-        render();
-    };
-
-    function render()
-    {
-        const pr = Renderer.DevicePixelRatio;
-        const cw = Math.round(pr * canvas.width * 0.5) * 2;
-        const ch = Math.round(pr * canvas.height * 0.5) * 2;
-
-        let dx = Math.round(-0.5 * textUniform.rectangle[2]);
-        let dy = Math.round( 0.5 * textUniform.rectangle[3]);
-
-        let ws = 2.0 / cw;
-        let hs = 2.0 / ch;
-
-        textUniform.transform = mat3.create(
-            ws,       0,         0,
-            0,        hs,        0,
-            dx * ws,  dy * hs,   1
-        );
-
-        Renderer.WriteBuffer(textUniform.buffer, textUniform.transform.buffer);
-
-        text.Render();
-    }
+        await loadFontTexture(Subtitle, RegularTexture);
+        Subtitle.Write("Unopinionated WebGPU Abstraction Library", [-215, 0]);
+        Title.Write("UWAL", [-185, 100]);
+        Title.Render(false);
+        Subtitle.Render();
+    });
 
     observer = new ResizeObserver(entries =>
     {
-        /* for (const entry of entries)
+        for (const entry of entries)
         {
             const { inlineSize, blockSize } = entry.contentBoxSize[0];
             Renderer.SetCanvasSize(inlineSize, blockSize);
-        } */
-
-        Renderer.SetCanvasSize(700, 400);
+        }
     });
 
     observer.observe(canvas);
