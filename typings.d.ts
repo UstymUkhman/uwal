b10a187773c71538e699025ea35dfcb2f1513ad0
Ustym <ustym.ukhman@gmail.com>
2025-07-05 20:49:24 +0300

    Init Render And Compute Stages

--- typings.d.ts
+++ typings.d.ts
@@ -6,5 +6,7 @@
 declare type ConfigurationOptions = import("./lib/Device").ConfigurationOptions;
 declare type LegacyComputation = import("./lib/Device").LegacyComputation;
 declare type LegacyRenderer = import("./lib/Device").LegacyRenderer;
+declare type Computation = import("./lib/Device").Computation;
+declare type Renderer = import("./lib/Device").Renderer;
 
 declare const GUI: Object;
