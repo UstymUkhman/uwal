79b71b6b3dbfd3cbfecda76bdfe119f7a4400194
Ustym Ukhman <ustym.ukhman@gmail.com>
2025-07-21 15:46:14 +0300

    Update GPUTiming Util Classes

--- src/lessons/timing-performance/NonNegativeRollingAverage.js
+++ src/lessons/timing-performance/NonNegativeRollingAverage.js
@@ -1,13 +1,3 @@
-/**
- * @module NonNegativeRollingAverage
- * @author Ustym Ukhman <ustym.ukhman@gmail.com>
- * @description We disallow negative values as this is used for timestamp queries
- * where it's possible for a query to return a beginning time greater than
- * the end time. {@link https://gpuweb.github.io/gpuweb/#timestamp}
- * @version 0.0.11
- * @license MIT
- */
-
 export default class NonNegativeRollingAverage
 {
     #count = 0;
