6dd7a6749b34741aed51274deee26807d213edfd
Ustym Ukhman <ustym.ukhman@gmail.com>
2025-08-06 19:57:48 +0300

    Update Lessons Build

---
+++ dist/lessons/assets/index-BA-HN0B3.js
@@ -0,0 +1,10 @@
+import{D as o}from"./index-sDmynApN.js";var g="@compute @workgroup_size(1)fn compute(@builtin(global_invocation_id)id: vec3u){let center=vec2f(textureDimensions(Texture))/2.0;let dist=distance(vec2f(id.xy),center);textureStore(Texture,id.xy,select(vec4f(1,0,0,1),vec4f(0,1,1,1),dist/32.0 % 2.0<1.0));}";/**
+ * @module Storage Textures
+ * @author Ustym Ukhman <ustym.ukhman@gmail.com>
+ * @description This lesson is reproduced from WebGPU Storage Textures
+ * {@link https://webgpufundamentals.org/webgpu/lessons/webgpu-storage-textures.html}&nbsp;
+ * and developed by using a version listed below. Please note that this code
+ * may be simplified in future thanks to more recent library APIs.
+ * @version 0.0.6
+ * @license MIT
+ */(async function(s){const u=await o.SetRequiredFeatures("bgra8unorm-storage"),n=o.PreferredCanvasFormat,i=u.has("bgra8unorm-storage")&&n==="bgra8unorm"?n:"rgba8unorm";n==="bgra8unorm"&&i==="rgba8unorm"&&console.warn('Preferred canvas format is "bgra8unorm", but since "bgra8unorm-storage" feature is not available on this device, a less performant "rgba8unorm" format will be used instead, in order to enable storage textures for this program.');let e,t;try{e=new(await o.ComputePipeline()),t=new(await o.RenderPipeline(s,"Storage Textures",{usage:GPUTextureUsage.TEXTURE_BINDING|GPUTextureUsage.STORAGE_BINDING}))}catch(r){alert(r)}const c=e.CreateShaderModule([`@group(0) @binding(0) var Texture: texture_storage_2d<${i}, write>;`,g]);e.CreatePipeline({module:c});function d(){const{width:r,height:a}=t.CurrentTexture;e.SetBindGroups(e.CreateBindGroup(e.CreateBindGroupEntries(t.CurrentTextureView))),e.Workgroups=[r,a],e.Compute(!0)}new ResizeObserver(r=>{for(const a of r){const{inlineSize:l,blockSize:m}=a.contentBoxSize[0];t.SetCanvasSize(l,m)}d()}).observe(document.body)})(document.getElementById("lesson"));
---
+++ dist/lessons/assets/index-CJQ4S_Lw.js
@@ -0,0 +1,10 @@
+import{D as s}from"./index-sDmynApN.js";import{T as f}from"./Triangle.vert-Y1l3LntC.js";var u="@fragment fn fragment(@builtin(position)position: vec4f)->@location(0)vec4f {let red=vec4f(1,0,0,1);let cyan=vec4f(0,1,1,1);let grid=vec2u(position.xy)/8;let checker=(grid.x+grid.y)% 2==1;return select(red,cyan,checker);}";/**
+ * @module Inter-stage Variables
+ * @author Ustym Ukhman <ustym.ukhman@gmail.com>
+ * @description This lesson is reproduced from WebGPU Inter-stage Variables
+ * {@link https://webgpufundamentals.org/webgpu/lessons/webgpu-inter-stage-variables.html}&nbsp;
+ * and developed by using a version listed below. Please note that this code
+ * may be simplified in future thanks to more recent library APIs.
+ * @version 0.0.4
+ * @license MIT
+ */(async function(r){let e;try{e=new(await s.RenderPipeline(r,"Checkerboard Triangle"))}catch(t){alert(t)}const n=e.CreatePassDescriptor(e.CreateColorAttachment(void 0,"clear","store",[.3,.3,.3,1])),o=e.CreateShaderModule(f),i=e.CreateShaderModule(u);e.CreatePipeline({vertex:e.CreateVertexState(o),fragment:e.CreateFragmentState(i)});function a(){e.SetCanvasSize(r.width,r.height),n.colorAttachments[0].view=e.CurrentTextureView,e.Render(3)}new ResizeObserver(t=>{for(const c of t){const{inlineSize:d,blockSize:l}=c.contentBoxSize[0];e.SetCanvasSize(d,l)}a()}).observe(document.body)})(document.getElementById("lesson"));
