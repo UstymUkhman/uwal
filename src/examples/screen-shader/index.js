7a15ca13143734ce3afa2418ca6a3eabb1a31d19
Ustym Ukhman <ustym.ukhman@gmail.com>
2025-08-27 17:34:54 +0300

    Update Screen Shader Example To `v0.1.0`

--- src/examples/screen-shader/index.js
+++ src/examples/screen-shader/index.js
@@ -5,70 +5,50 @@
  * {@link https://oframe.github.io/ogl/examples/?src=triangle-screen-shader.html}&nbsp;
  * and developed by using a version listed below. Please note that this code
  * may be simplified in future thanks to more recent library APIs.
- * @version 0.0.4
+ * @version 0.1.0
  * @license MIT
  */
 
-import { UWAL, Shaders } from "#/index";
+import { Device, Shaders, Color } from "#/index";
 import ScreenShader from "./ScreenShader.wgsl";
 
-let screenUniformBuffer;
 /** @type {number} */ let raf;
+/** @type {Renderer} */ let Renderer;
+/** @type {GPUBuffer} */ let screenBuffer;
 /** @type {ResizeObserver} */ let observer;
-
-/** @type {InstanceType<Awaited<ReturnType<UWAL.RenderPipeline>>>} */ let Renderer;
 
 /** @param {HTMLCanvasElement} canvas */
 export async function run(canvas)
 {
     try
     {
-        Renderer = new (await UWAL.RenderPipeline(canvas, "Screen Shader"));
+        Renderer = new (await Device.Renderer(canvas, "Screen Shader"));
     }
     catch (error)
     {
         alert(error);
     }
 
-    const descriptor = Renderer.CreatePassDescriptor(Renderer.CreateColorAttachment());
-    const module = Renderer.CreateShaderModule([Shaders.Quad, ScreenShader]);
+    const RenderPipeline = await Renderer.CreatePipeline([Shaders.Quad, ScreenShader]);
+    const { screen, buffer } = RenderPipeline.CreateUniformBuffer("screen");
 
-    Renderer.CreatePipeline({
-        vertex: Renderer.CreateVertexState(module),
-        fragment: Renderer.CreateFragmentState(module)
-    });
+    RenderPipeline.SetBindGroups(RenderPipeline.CreateBindGroup(
+        RenderPipeline.CreateBindGroupEntries(buffer)
+    ));
 
-    const screenUniformBufferSize =
-        Float32Array.BYTES_PER_ELEMENT +
-        Float32Array.BYTES_PER_ELEMENT * 3;
+    screen.color.set(new Color(0x005a9c).rgb);
+    RenderPipeline.SetDrawParams(6);
+    screenBuffer = buffer;
 
-    screenUniformBuffer = Renderer.CreateBuffer({
-        size: screenUniformBufferSize,
-        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
-    });
-
-    const bindGroup = Renderer.CreateBindGroup(
-        Renderer.CreateBindGroupEntries({ buffer: screenUniformBuffer })
-    );
-
-    Renderer.SetBindGroups(bindGroup);
-
-    const screenUniformValues = new Float32Array(
-        screenUniformBufferSize / Float32Array.BYTES_PER_ELEMENT
-    );
-
-    screenUniformValues.set([0, 0.3515625, 0.609375]);
-
-    /** @param {DOMHighResTimeStamp} time */
     function render(time)
     {
+        screen.time.set([time * 0.001]);
         raf = requestAnimationFrame(render);
-        screenUniformValues.set([time * 0.001], 3);
 
-        descriptor.colorAttachments[0].view = Renderer.CurrentTextureView;
-        Renderer.WriteBuffer(screenUniformBuffer, screenUniformValues);
-
-        Renderer.Render(6);
+        // By passing `screen.time.buffer`, we're also
+        // writing the `screen.color` value to the GPUBuffer:
+        Renderer.WriteBuffer(buffer, screen.time.buffer);
+        Renderer.Render();
     }
 
     observer = new ResizeObserver(entries =>
@@ -89,10 +69,9 @@ export async function run(canvas)
 
 export function destroy()
 {
-    UWAL.OnDeviceLost = () => void 0;
-    screenUniformBuffer?.destroy();
+    Device.OnLost = () => void 0;
+    Device.Destroy(screenBuffer);
     cancelAnimationFrame(raf);
     observer.disconnect();
     Renderer.Destroy();
-    UWAL.Destroy();
 }
