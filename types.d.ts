602b4c830090f0c833564088926ab2690201d228
UstymUkhman <ustym.ukhman@gmail.com>
2025-09-17 16:05:43 +0300

    Add Node Primitive

--- types.d.ts
+++ types.d.ts
@@ -5,6 +5,7 @@
 /// <reference types="vite-plugin-glsl/ext" />
 
 export { USAGE, BLEND_STATE } from "./types/pipelines/Constants";
+export { Node, Mesh, Shape } from "./types/primitives";
 export * as TEXTURE from "./types/textures/Constants";
 export { GPUTiming, MathUtils } from "./types/utils";
 export { default as Device } from "./types/Device";
@@ -13,7 +14,6 @@ export * as Geometries from "./types/geometries";
 export * as Materials from "./types/materials";
 export { ERROR_CAUSE } from "./types/Errors";
 export * as Shaders from "./types/shaders";
-export { Shape } from "./types/primitives";
 /** @deprecated `SDFText` will be replaced by `MSDFText`. */
 export { SDFText } from "./types/text";
 
