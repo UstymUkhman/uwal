9df131c533e2e8868dbc65ba897e6f39f22e5d6b
UstymUkhman <ustym.ukhman@gmail.com>
2025-10-26 14:03:28 +0200

    Update Internal Pipeline Types

---
+++ lib/types.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="wgpu-matrix" />
+/// <reference types="@webgpu/types" />
+
+declare type Renderer = import("#/stages/RenderStage").default;
+declare type Computation = import("#/stages/ComputeStage").default;
+declare type RenderPipeline = import("#/pipelines/RenderPipeline").RenderPipeline;
+declare type ComputePipeline = import("#/pipelines/ComputePipeline").ComputePipeline;
