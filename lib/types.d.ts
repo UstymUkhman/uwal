386bd9323913d5f935a901ca0125fdc35e067b45
UstymUkhman <ustym.ukhman@gmail.com>
2025-10-28 09:52:55 +0200

    Improve Types Generation

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,5 +1,8 @@
+declare const VERSION: string;
+
 /// <reference types="wgpu-matrix" />
 /// <reference types="@webgpu/types" />
+/// <reference types="vite-plugin-glsl/ext" />
 
 declare type Renderer = import("#/stages/RenderStage").default;
 declare type Computation = import("#/stages/ComputeStage").default;
