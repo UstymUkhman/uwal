1c0a23f77279cf07b678336a1f31ffa3e456d58e
UstymUkhman <ustym.ukhman@gmail.com>
2025-11-05 18:52:06 +0200

    Update Pipeline Types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -6,5 +6,5 @@ declare const VERSION: string;
 
 declare type Renderer = import("#/stages/RenderStage").default;
 declare type Computation = import("#/stages/ComputeStage").default;
-declare type RenderPipeline = import("#/pipelines/RenderPipeline").RenderPipeline;
-declare type ComputePipeline = import("#/pipelines/ComputePipeline").ComputePipeline;
+declare type RenderPipeline = import("#/pipelines/RenderPipeline").RenderPipelineInstance;
+declare type ComputePipeline = import("#/pipelines/ComputePipeline").ComputePipelineInstance;
