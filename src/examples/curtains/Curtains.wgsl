b6e04b2aba7519196486a2e869a6e2bd358ca748
Ustym Ukhman <ustym.ukhman@gmail.com>
2026-05-07 18:45:35 +0300

    Add Text To Curtains Example

--- src/examples/curtains/Curtains.wgsl
+++ src/examples/curtains/Curtains.wgsl
@@ -10,15 +10,17 @@ struct Plane
     @builtin(position) position: vec4f,
     @location(0) originalPosition: vec4f,
     @location(1) vertexPosition: vec4f,
-    @location(2) textureCoords: vec2f
+    @location(2) textureCoords: vec2f,
+    @location(3) textCoords: vec2f
 };
 
 const LAVENDER = vec3f(0.9, 0.9, 0.98);
 const LAVENDER_BLUSH = vec3f(1, 0.94, 0.96);
 
 @group(0) @binding(0) var Sampler: sampler;
-@group(0) @binding(1) var Logo: texture_2d<f32>;
-@group(0) @binding(2) var<uniform> curtains: Curtains;
+@group(0) @binding(1) var Text: texture_2d<f32>;
+@group(0) @binding(2) var Logo: texture_2d<f32>;
+@group(0) @binding(3) var<uniform> curtains: Curtains;
 
 fn getNormal(o: vec3f, p: vec3f) -> vec3f
 {
@@ -56,23 +58,36 @@ fn getNormal(o: vec3f, p: vec3f) -> vec3f
     vertex.x += strength * abs(ratio) * sign(vertex.x) / 160;
     vertex.z += strength / 30;
 
-    return Plane(GetVertexClipSpace(vertex), position, vertex, uv);
+    var plane: Plane;
+    var textCoords = (vertex.xy + 1) * 0.5;
+    textCoords = vec2f(textCoords.x, 1 - textCoords.y);
+
+    plane.position = GetVertexClipSpace(vertex);
+    plane.originalPosition = position;
+    plane.vertexPosition = vertex;
+    plane.textCoords = textCoords;
+    plane.textureCoords = uv;
+
+    return plane;
 }
 
 @fragment fn fragment(plane: Plane) -> @location(0) vec4f
 {
+    // Background Gradient:
     let time = sin(curtains.deltaTime.y * 0.005);
     let top = mix(LAVENDER, LAVENDER_BLUSH, time);
     let bottom = mix(LAVENDER, LAVENDER_BLUSH, -time);
 
-    let coords = plane.textureCoords;
-    let center = vec2f(0.5);
-    let scale = 1 / vec2f(0.5 / curtains.planeRatio, 0.5);
-    let uv = (coords - center) * scale + center;
-
+    var coords = plane.textureCoords;
     let s = smoothstep(0f, 1f, coords.y);
     let background = mix(top, bottom, s);
+
+    // UWAL Logo:
+    let center = vec2f(0.5, 0.75);
+    let scale = 1 / vec2f(0.5 / curtains.planeRatio, 0.5);
+    let uv = (coords - center) * scale + center;
     let logo = textureSample(Logo, Sampler, uv);
+
     var color = vec4f(mix(background, logo.rgb, logo.a), 1);
 
     if (uv.x < 0 || uv.x > 1 || uv.y < 0 || uv.y > 1)
@@ -80,9 +95,14 @@ fn getNormal(o: vec3f, p: vec3f) -> vec3f
         color.rgb = background;
     }
 
+    // Text Texture:
+    coords = plane.textCoords * 3 - 1;
+    let text = textureSample(Text, Sampler, coords);
+    color.rgb = mix(color.rgb, text.rgb, text.a);
+
+    // Lighting:
     let intensity = 0.35;
     let ambient = color.rgb * (1 - intensity);
-
     let normal = getNormal(plane.originalPosition.xyz, plane.vertexPosition.xyz);
     let light = smoothstep(0.45, 1, dot(normal, normalize(vec3f(0.3, 0.3, 1))));
 
--- src/examples/curtains/index.js
+++ src/examples/curtains/index.js
@@ -15,6 +15,7 @@ import {
     Device,
     Shaders,
     BINDINGS,
+    MSDFText,
     MathUtils,
     Geometries,
     PerspectiveCamera
@@ -22,9 +23,12 @@ import {
 
 import Curtains from "./Curtains.wgsl";
 import Logo from "/assets/images/logo.png";
+import Roboto from "/assets/fonts/Roboto-Regular.json?url";
 
 /** @type {number} */ let raf;
 /** @type {Renderer} */ let Renderer;
+/** @type {GPUBuffer} */ let textBuffer;
+/** @type {GPUTexture} */ let textTexture;
 /** @type {ResizeObserver} */ let observer;
 /** @type {GPUBuffer} */ let curtainsBuffer;
 
@@ -42,54 +46,93 @@ export async function run(canvas)
 
     const Pipeline = new Renderer.Pipeline();
     const Camera = new PerspectiveCamera(35);
-    const Geometry = new Geometries.Mesh();
 
+    const Geometry = new Geometries.Mesh();
     let maxDelta = 4, delta = 0, time = 0;
+
     const scene = new Scene("Curtains");
     const Plane = new Mesh(Geometry);
+    const Text = new MSDFText();
 
     const module = Pipeline.CreateShaderModule([Shaders.MeshVertex, Curtains]);
     const { curtains, buffer } = Pipeline.CreateUniformBuffer("curtains");
     Geometry.Primitive = Geometries.Primitives.plane({ nx: 50, ny: 37 });
 
+    const TextPipeline = await Text.SetRenderPipeline(Renderer, {
+        multisample: Pipeline.CreateMultisampleState()
+    });
+
+    const cameraBuffer = Camera.SetRenderPipeline(Pipeline);
     const Texture = new (await Device.Texture(Renderer));
+
     const logo = await Texture.CopyImageToTexture(
         await Texture.CreateImageBitmap(Logo),
         { mipmaps: false }
     );
 
-    Plane.SetRenderPipeline(await Renderer.AddPipeline(Pipeline,
-        {
-            primitive: Pipeline.CreatePrimitiveState(),
-            fragment: Pipeline.CreateFragmentState(module),
-            multisample: Pipeline.CreateMultisampleState(),
-            depthStencil: Pipeline.CreateDepthStencilState(),
-            vertex: Pipeline.CreateVertexState(module, "planeVertex", [
-                Geometry.GetPositionBufferLayout(Pipeline),
-                Geometry.GetUVBufferLayout(Pipeline, "planeVertex")
-            ])
-        }), [
-            Camera.SetRenderPipeline(Pipeline),
-            Texture.CreateSampler({ filter: "linear" }),
-            logo.createView(),
-            buffer
-        ],
-        [BINDINGS.CAMERA_MATRIX, 0]
-    );
-
-    canvas.removeEventListener("mousemove", onMove);
-    canvas.removeEventListener("touchmove", onMove);
-    canvas.addEventListener("mousemove", onMove);
-    canvas.addEventListener("touchmove", onMove);
+    // canvas.removeEventListener("mousemove", onMove);
+    // canvas.removeEventListener("touchmove", onMove);
+    // canvas.addEventListener("mousemove", onMove);
+    // canvas.addEventListener("touchmove", onMove);
 
     const mousePosition = MathUtils.Vec2.create();
     const lastPosition = MathUtils.Vec2.create();
-    const [x, y] = Plane.Scaling = [1.5, 0.9, 1];
 
-    curtains.planeRatio.set([x / y]);
+    await Text.LoadFont(Roboto);
     scene.AddMainCamera(Camera);
     curtainsBuffer = buffer;
     scene.Add(Plane);
+
+    function clear()
+    {
+        Text.Clear(textBuffer);
+        textTexture?.destroy();
+        cancelAnimationFrame(raf);
+    }
+
+    async function start()
+    {
+        const [width] = Renderer.BaseCanvasSize;
+        const size = (width - 360) / 1268;
+        const scaleX = size * 1.12;
+        const textY = size * 1.25;
+        const textZ = size * 4.6;
+
+        TextPipeline.SetTextureView(textTexture = Texture.CreateStorageTexture({
+            usage: GPUTextureUsage.RENDER_ATTACHMENT
+        }));
+
+        TextPipeline.DestroyPassEncoder = !!(Text.CameraMatrixBuffer = cameraBuffer);
+        const [x, y] = Plane.Scaling = [scaleX + 0.38 /* 0.36 */, 0.9, 1];
+        curtains.planeRatio.set([x / y]);
+
+        Text.SetTranslation(
+            MathUtils.Mat4.translation([0, textY - 2, textZ - 9]),
+            textBuffer = Text.Write("Unopinionated WebGPU Abstraction Library", 0, 0.0025, true)
+        );
+
+        Plane.SetRenderPipeline(await Renderer.AddPipeline(Pipeline,
+            {
+                primitive: Pipeline.CreatePrimitiveState(),
+                fragment: Pipeline.CreateFragmentState(module),
+                multisample: Pipeline.CreateMultisampleState(),
+                depthStencil: Pipeline.CreateDepthStencilState(),
+                vertex: Pipeline.CreateVertexState(module, "planeVertex", [
+                    Geometry.GetPositionBufferLayout(Pipeline),
+                    Geometry.GetUVBufferLayout(Pipeline, "planeVertex")
+                ])
+            }), [
+                cameraBuffer,
+                Texture.CreateSampler({ filter: "linear" }),
+                TextPipeline.TextureView,
+                logo.createView(),
+                buffer
+            ],
+            [BINDINGS.CAMERA_MATRIX, 0]
+        );
+
+        raf = requestAnimationFrame(render);
+    }
 
     function onMove()
     {
@@ -121,8 +164,13 @@ export async function run(canvas)
         curtains.deltaTime.set([delta, time++]);
         Pipeline.WriteBuffer(buffer, curtains.deltaTime.buffer);
 
-        raf = requestAnimationFrame(render);
+        Pipeline.Active = false;
+        Renderer.Render(false);
+
+        Pipeline.Active = true;
         Renderer.Render(scene);
+
+        // requestAnimationFrame(render);
     }
 
     observer = new ResizeObserver(entries =>
@@ -138,8 +186,7 @@ export async function run(canvas)
             Camera.UpdateWorldMatrix(true);
         }
 
-        cancelAnimationFrame(raf);
-        raf = requestAnimationFrame(render);
+        clear(), start();
     });
 
     observer.observe(document.body);
@@ -151,5 +198,8 @@ export function destroy()
     cancelAnimationFrame(raf);
     observer.disconnect();
     Renderer.Destroy();
-    Device.Destroy(curtainsBuffer);
+    Device.Destroy([
+        curtainsBuffer,
+        textBuffer
+    ], textTexture);
 }
