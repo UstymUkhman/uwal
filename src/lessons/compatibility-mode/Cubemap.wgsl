514a31027f9053fd69cb773e497522291f3ae7d9
Ustym Ukhman <ustym.ukhman@gmail.com>
2026-05-30 16:54:40 +0300

    Update Compatibility Mode Lesson

---
+++ src/lessons/compatibility-mode/Cubemap.wgsl
@@ -0,0 +1,7 @@
+@group(0) @binding(0) var Sampler: sampler;
+@group(0) @binding(1) var Texture: texture_2d<f32>;
+
+@fragment fn fragment(cube: MeshVertexUV) -> @location(0) vec4f
+{
+    return textureSample(Texture, Sampler, cube.uv);
+}
--- src/lessons/compatibility-mode/index.js
+++ src/lessons/compatibility-mode/index.js
@@ -9,11 +9,25 @@
  * @license MIT
  */
 
-import { Device } from "#/index";
+import {
+    Mesh,
+    Scene,
+    Device,
+    Shaders,
+    BINDINGS,
+    MathUtils,
+    Geometries,
+    PerspectiveCamera
+} from "#/index";
+
+import Cubemap from './Cubemap.wgsl';
+import Ground from "/assets/images/leadenhall/neg-y.jpg";
 
 (async function(canvas)
 {
     /** @type {Renderer} */ let Renderer;
+
+    Device.AdapterOptions = { featureLevel: "compatibility", forceCompatibility: true };
 
     try
     {
@@ -24,13 +38,85 @@ import { Device } from "#/index";
         alert(error);
     }
 
+    const CubePipeline = new Renderer.Pipeline();
+    const CubeGeometry = new Geometries.Mesh();
+    const Camera = new PerspectiveCamera();
+    const Cube = new Mesh(CubeGeometry);
+    const scene = new Scene();
+    scene.Add(Cube);
+
+    const radToDeg =
+    {
+        converters: GUI.converters.radToDeg,
+        min: -360,
+        max: 360,
+        step: 1
+    };
+
+    const settings =
+    {
+        rotation:
+        [
+            MathUtils.DegreesToRadians(20),
+            MathUtils.DegreesToRadians(25),
+            MathUtils.DegreesToRadians(0)
+        ]
+    };
+
+    const gui = new GUI();
+    gui.onChange(render);
+
+    gui.add(settings.rotation, "0", radToDeg).name("rotation.x");
+    gui.add(settings.rotation, "1", radToDeg).name("rotation.y");
+    gui.add(settings.rotation, "2", radToDeg).name("rotation.z");
+
+    const module = CubePipeline.CreateShaderModule([Shaders.MeshVertex, Cubemap]);
+    CubeGeometry.Primitive = Geometries.Primitives.cube();
+    Cube.Transform = [void 0, settings.rotation, 2];
+
+    const Texture = new (await Device.Texture(Renderer));
+    const texture = await Texture.CopyImageToTexture(
+        await Texture.CreateImageBitmap(Ground)
+    );
+
+    Cube.SetRenderPipeline(await Renderer.AddPipeline(CubePipeline,
+        {
+            primitive: CubePipeline.CreatePrimitiveState(),
+            depthStencil: CubePipeline.CreateDepthStencilState(),
+            fragment: CubePipeline.CreateFragmentState(module),
+            vertex: CubePipeline.CreateVertexState(module, "vertexUV", [
+                CubeGeometry.GetPositionBufferLayout(CubePipeline),
+                CubePipeline.CreateVertexBufferLayout("uv", "vertexUV")
+            ])
+        }), [
+            Texture.CreateSampler({ filter: "linear" }),
+            texture.createView(/* { dimension: "cube" } */),
+            Camera.SetRenderPipeline(CubePipeline)
+        ], [0, 1, BINDINGS.CAMERA_MATRIX]
+    );
+
+    CubeGeometry.AddUVBuffer(CubePipeline, CubeGeometry.Primitive.uvs);
+
+    function render()
+    {
+        Cube.Rotation = settings.rotation;
+        Renderer.Render(scene);
+    }
+
     const observer = new ResizeObserver(entries =>
     {
         for (const entry of entries)
         {
             const { inlineSize, blockSize } = entry.contentBoxSize[0];
             Renderer.SetCanvasSize(inlineSize, blockSize);
+            Camera.AspectRatio = Renderer.AspectRatio;
+            Camera.Position = [0, 1, 5];
+            Camera.LookAt([0, 0, 0]);
+            scene.AddMainCamera(Camera);
+            Camera.UpdateWorldMatrix(true);
         }
+
+        render();
     });
 
     observer.observe(document.body);
