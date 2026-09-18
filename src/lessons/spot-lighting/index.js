940d625111366164250eaa56902c32f7c135e0b3
Ustym Ukhman <ustym.ukhman@gmail.com>
2026-09-18 12:17:32 +0300

    Update From Main

--- src/lessons/spot-lighting/index.js
+++ src/lessons/spot-lighting/index.js
@@ -26,9 +26,9 @@ import createVertices from "../directional-lighting/F.js";
         alert(error);
     }
 
-    const FGeometry = new UWAL.MeshGeometry();
     const Camera = new UWAL.PerspectiveCamera();
     const FPipeline = new Renderer.Pipeline();
+    const FGeometry = new UWAL.MeshGeometry();
     const FMesh = new UWAL.Mesh(FGeometry);
 
     const scene = new UWAL.Scene();
